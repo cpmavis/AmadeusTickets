822f74a5fa7e2a778fc3afa94ff67982618e653e
Corey <cpmavis78@gmail.com>
2023-09-15 19:40:25 -0500

    React Frontend

---
+++ app/src/main/ui/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import React, { useState } from 'react';
+import Locate from './components/Locate';
+import Flight from './components/Flight';
+import Confirm from './components/Confirm';
+import Order from './components/Order';
+
+function App() {
+  const [origin, setOrigin] = useState();
+  const [destination, setDestination] = useState();
+  const [flight, setFlight] = useState();
+  const [confirmation, setConfirmation] = useState();
+  const [order, setOrder] = useState();
+
+  return (
+    <div>
+      <Locate handleChoice={setDestination} display={"Origin"}/>
+      <Locate handleChoice={setOrigin} display={"Destination"}/>
+      { origin && 
+        destination && 
+        <Flight origin={origin} destination={destination} setFlight={setFlight}/> 
+      }
+      { flight &&
+        <Confirm flight={flight} setConfirmation={setConfirmation} /> 
+      }
+      { confirmation &&
+        <Order confirmation={confirmation} order={order} setOrder={setOrder} /> 
+      }
+    </div>
+  )
+}
+
+export default App;
