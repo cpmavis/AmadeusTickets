1701757c07ea1e2afab8ed8307d48d33ab5cddca
Corey <cpmavis78@gmail.com>
2023-09-15 19:22:28 -0500

    Java Backend

---
+++ app/src/main/java/com/tickets/app/AmadeusConnect.java
@@ -0,0 +1,36 @@
+package com.tickets.app;
+
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.referencedata.Locations;
+import com.amadeus.resources.FlightOfferSearch;
+import com.amadeus.resources.FlightPrice;
+import com.amadeus.resources.Location;
+
+enum AmadeusConnect {
+    INSTANCE;
+    private Amadeus amadeus;
+    private AmadeusConnect() {
+        this.amadeus = Amadeus
+                .builder("ZOeNrlzqfxXaz6yvh0wENMSs7LCjuZrf", "bWUClrzk9jiUcrpQ")
+                .build();
+    }
+    public Location[] locations(String keyword) throws ResponseException {
+        return amadeus.referenceData.locations.get(Params
+                .with("keyword", keyword)
+                .and("subType", Locations.AIRPORT));
+    }
+    public FlightOfferSearch[] flights(String origin, String destination, String departDate, String adults, String returnDate) throws ResponseException {
+        return amadeus.shopping.flightOffersSearch.get(
+                Params.with("originLocationCode", origin)
+                        .and("destinationLocationCode", destination)
+                        .and("departureDate", departDate)
+                        .and("returnDate", returnDate)
+                        .and("adults", adults)
+                        .and("max", 3));
+    }
+    public FlightPrice confirm(FlightOfferSearch offer) throws ResponseException {
+        return amadeus.shopping.flightOffersSearch.pricing.post(offer);
+    }
+}
---
+++ app/src/main/java/com/tickets/app/ApiController.java
@@ -0,0 +1,36 @@
+package com.tickets.app;
+
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.FlightOfferSearch;
+import com.amadeus.resources.FlightPrice;
+import com.amadeus.resources.Location;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api")
+public class ApiController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World Travel";
+    }
+
+    @GetMapping("/locations")
+    public Location[] locations(@RequestParam(required = true) String keyword) throws ResponseException {
+        return AmadeusConnect.INSTANCE.locations(keyword);
+    }
+
+    @GetMapping("/flights")
+    public FlightOfferSearch[] flights(@RequestParam(required = true) String origin,
+                                       @RequestParam(required = true) String destination,
+                                       @RequestParam(required = true) String departDate,
+                                       @RequestParam(required = true) String adults,
+                                       @RequestParam(required = false) String returnDate)
+            throws ResponseException {
+        return AmadeusConnect.INSTANCE.flights(origin, destination, departDate, adults, returnDate);
+    }
+
+    @PostMapping("/confirm")
+    public FlightPrice confirm(@RequestBody(required = true) FlightOfferSearch search) throws ResponseException {
+        return AmadeusConnect.INSTANCE.confirm(search);
+    }
+}
