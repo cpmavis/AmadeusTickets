9271969151589a6f62d44641c45cb0f65f1b2218
Corey <cpmavis78@gmail.com>
2023-09-29 16:23:03 -0500

    Need to get it in USD

--- app/src/main/ui/src/App.js
+++ app/src/main/ui/src/App.js
@@ -3,28 +3,21 @@ import React, { useState } from 'react';
 import Locate from './components/Locate';
 import Flight from './components/Flight';
 import Confirm from './components/Confirm';
-import Order from './components/Order';
 
 function App() {
   const [origin, setOrigin] = useState();
   const [destination, setDestination] = useState();
   const [flight, setFlight] = useState();
   const [confirmation, setConfirmation] = useState();
-  const [order, setOrder] = useState();
+  const [price, setPrice] = useState();
+ 
 
   return (
     <div>
       <Locate handleChoice={setDestination} display={"Origin"}/>
       <Locate handleChoice={setOrigin} display={"Destination"}/>
-      
-      { origin && 
-        destination && 
-        <Flight origin={origin} destination={destination} setFlight={setFlight}/> 
-      }
-      { flight &&
-        <Confirm flight={flight} confirmation={confirmation} setConfirmation={setConfirmation} /> 
-      }
-
+      <Flight origin={origin} destination={destination} setFlight={setFlight}/>
+      <Confirm flight={flight} confirmation={confirmation} setConfirmation={setConfirmation} /> 
     </div>
   )
   
