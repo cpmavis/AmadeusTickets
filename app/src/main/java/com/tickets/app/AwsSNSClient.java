c29215775106d40aa9dec03b6c4a73bd64c760a7
Corey <cpmavis78@gmail.com>
2023-10-02 15:55:41 -0500

    AWS notification needs to be set up

---
+++ app/src/main/java/com/tickets/app/AwsSNSClient.java
@@ -0,0 +1,10 @@
+package com.tickets.app;
+
+public class AwsSNSClient {
+    public static final String AWS_ACCESS_KEY_ID = "aws.accessKeyID";
+    public static final String AWS_SECRET_KEY = "aws.secretKey";
+    static {
+        System.setProperty(AWS_ACCESS_KEY_ID, );
+        System.setProperty(AWS_SECRET_KEY );
+    }
+}
